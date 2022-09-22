c8557b28f93f14dd0689d6f6fb4063f280b1336b
baba-s <shotababa1206@gmail.com>
2022-09-22 19:40:09 +0900

    add

---
+++ Editor/CompletableProcessBuildWithReportBase.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+
+namespace Kogane
+{
+    /// <summary>
+    /// ビルドに失敗した時のコールバックも実装できる IPreprocessBuildWithReport / IPostprocessBuildWithReport
+    /// OnSuccess の後に OnComplete が呼び出されます
+    /// OnSuccess はビルドに成功した時にのみ呼び出されます
+    /// OnComplete はビルドに成功した時も失敗した時も呼び出されます
+    /// </summary>
+    public abstract class CompletableProcessBuildWithReportBase :
+        IPreprocessBuildWithReport,
+        IPostprocessBuildWithReport
+    {
+        int IOrderedCallback.callbackOrder => CallbackOrder;
+
+        void IPreprocessBuildWithReport.OnPreprocessBuild( BuildReport report )
+        {
+            void OnUpdate()
+            {
+                if ( BuildPipeline.isBuildingPlayer ) return;
+                EditorApplication.update -= OnUpdate;
+                OnComplete();
+            }
+
+            EditorApplication.update += OnUpdate;
+            OnStart( report );
+        }
+
+        void IPostprocessBuildWithReport.OnPostprocessBuild( BuildReport report )
+        {
+            OnSuccess( report );
+        }
+
+        protected virtual int CallbackOrder => 0;
+
+        protected virtual void OnStart( BuildReport   report ) { }
+        protected virtual void OnSuccess( BuildReport report ) { }
+        protected virtual void OnComplete()                    { }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.CompletableProcessBuildWithReportBase
+# Kogane Completable Process Build With Report Base
+
+ビルドに失敗した時のコールバックも実装できる IPreprocessBuildWithReport / IPostprocessBuildWithReport
